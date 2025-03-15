6f98d023d0c77fba1d6a3ba0244c26790192af9b
[Swetha701] <[mswetha@student.tce.edu]>
2025-03-15 20:02:39 +0530

    Added solution for Commit-X Question 3

---
+++ Solution3.py
@@ -0,0 +1,10 @@
+def find_single_number(arr):
+    result = 0
+    for num in arr:
+        result ^= num 
+    return result
+
+n = int(input()) 
+arr = list(map(int, input().split()))  
+
+print(find_single_number(arr))
