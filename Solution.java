816749e2d57209f396470e3bb12b31a20483e234
VENKATESH-BABU09 <rpvk09082004@gmail.com>
2025-03-15 14:01:45 +0530

    Added Solution to the problem

---
+++ Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i],map.get(arr[i])+1);
+            }else{
+                map.put(arr[i],1);
+            }
+        }
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            if(entry.getValue()==1){
+                System.out.print(entry.getKey());
+                break;
+                
+            }
+        }
+        
+
+
+    }
+}
